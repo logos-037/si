d43cec2f4be5ee036b8a29e9bca7cbdb4eb598af
Miquel Barja Romero <logos@gmail.com>
2021-02-23 12:09:41 +0100

    Fitxer nou brrr

---
+++ classes.py
@@ -0,0 +1,13 @@
+class classe:
+    
+    def __init__(self):
+        self.x = []
+    
+    def __eq__(self):
+        pass
+    
+    def __repr__():
+        pass
+    
+    def primer():
+        pass
